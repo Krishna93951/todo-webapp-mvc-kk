a4fc79e00e0b5af16c3246f36527a74507a7442c
Krishna93951 <kkagile123@gmail.com>
2019-10-04 18:05:23 +0530

    1 file modified

--- model.js
+++ model.js
@@ -1,45 +1,25 @@
-function getTodo() 
+var todos = [];
+var todoSession = [];
+var todos_str,todos_strSession;
+
+//Local Storage
+function getTodoFromLocal() 
 {
-  var todos_str = localStorage.getItem('todo');
-  if (todos_str != null) 
-  {
-    todos = JSON.parse(todos_str);
-  }
+  todos = JSON.parse(localStorage.getItem('todo')) || [];
   return todos;
 }
 
-function addTask() 
-{ 
-  var task = document.getElementById('todoTask').value;
-  getTodo();
-  todos.push(task);
+function setTodoToLocal(){
   localStorage.setItem('todo', JSON.stringify(todos));
-  showTask();
-  totalMsg()
-  focusField();
-  clearField();
-  
 }
 
-function removeTask() 
+//Seesion Storage
+function getTodoFromSession() 
 {
-  var id = this.getAttribute('id');
-  getTodo();
-  todos.splice(id, 1);
-  localStorage.setItem('todo', JSON.stringify(todos));
-  showTask();
-  totalMsg()
+  todoSession = JSON.parse(sessionStorage.getItem('todosession')) || [];
+  return todoSession ;
 }
 
-function totalMsg(){
-  getTodo();
-  var completed = document.querySelectorAll('input[type="checkbox" ]:checked')
-  console.log(Object.values(completed))
-  var pending = todos.length-completed.length;
-  if(todos.length === 1){
-    return totalTodos.innerHTML =   countMessage.all +todos.length +" "+countMessage.completed+completed.length+" "+  countMessage.pending+pending ; 
-  }
-  else{
-    return totalTodos.innerHTML = countMessage.all +todos.length +" "+countMessage.completed+completed.length+" "+  countMessage.pending+pending ;
-    }
+function setTodoToSession(){
+  sessionStorage.setItem('todosession', JSON.stringify(todoSession));
 }
